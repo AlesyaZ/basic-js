42d77c0d0e91acb29ed7905d759b1aadf4b864ea
Alesya <alesya.zarembo@mail.ru>
2020-03-21 04:52:55 +0300

    feat: add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,26 +1,26 @@
-module.exports = function transform(/*arr*/) {
+module.exports = function transform(arr) {
     if (!Array.isArray(arr)) {
         throw new Error();
     }
        let transformArr = [];
    
-   for ( let i=0; i < arr.legth; i++) {
+   for ( let i=0; i < arr.length; i++) {
        if (arr[i] == '--discard-next') {
            if ( i < arr.length - 1 ) {
              i++;;
            }
    }   else if (arr[i] == '--discard-prev') {
            if (i > 0) {
-            transformArray.push(arr[i - 1]);
+            transformArr.pop();
        }
    }   else if (arr[i] == '--double-next') {
            if (i < arr.length - 1){
            transformArr.push(arr[i + 1]);
            }
-   }   else if(arr[i] == '--double-prev') {
-           if (i > 0) {
-           transformArr.pop();
-       }
+   }  else if (arr[i] == '--double-prev') {
+             if (i > 0) {
+            transformArr.push(arr[i - 1]);
+        } 
    }
        else {
            transformArr.push(arr[i]);
