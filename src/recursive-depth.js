6fa353272b2a5cae7c4b072c3faac08ce360c4ce
Alesya <alesya.zarembo@mail.ru>
2020-03-21 04:39:07 +0300

    feat: add solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,8 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+    if(arr.every(i => !Array.isArray(i))) {
+         return 1;
+        } 
+     return 1 + this.calculateDepth(arr.flat());
     }
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,32 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform(/*arr*/) {
+    if (!Array.isArray(arr)) {
+        throw new Error();
+    }
+       let transformArr = [];
+   
+   for ( let i=0; i < arr.legth; i++) {
+       if (arr[i] == '--discard-next') {
+           if ( i < arr.length - 1 ) {
+             i++;;
+           }
+   }   else if (arr[i] == '--discard-prev') {
+           if (i > 0) {
+            transformArray.push(arr[i - 1]);
+       }
+   }   else if (arr[i] == '--double-next') {
+           if (i < arr.length - 1){
+           transformArr.push(arr[i + 1]);
+           }
+   }   else if(arr[i] == '--double-prev') {
+           if (i > 0) {
+           transformArr.pop();
+       }
+   }
+       else {
+           transformArr.push(arr[i]);
+       }
+       
+   }
+    return transformArr;
+   };
+ 
