0a3f5b751c488b49ea64d34452239a2af232436a
Alesya <alesya.zarembo@mail.ru>
2020-09-12 23:48:45 +0300

    fix: new solution and change test

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,6 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed ) {
     let turns = 2 ** disksNumber -1;
-    let seconds = turns / (turnsSpeed / 3600);
-    return {turns, seconds};
+    let seconds = Math.floor(turns / (turnsSpeed / 3600));
+    const resultCalculate = {turns, seconds};
+    return resultCalculate;
 }
